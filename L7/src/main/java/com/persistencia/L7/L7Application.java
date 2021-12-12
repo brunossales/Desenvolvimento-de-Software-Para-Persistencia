3e9014d9ac2dabfd4be34bed1ea13f95c7827115
brunossales <bruno.particular25@alu.ufc.br>
2021-12-12 10:19:59 -0300

    up

---
+++ L7/src/main/java/com/persistencia/L7/L7Application.java
@@ -0,0 +1,13 @@
+package com.persistencia.L7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L7Application.class, args);
+	}
+
+}
